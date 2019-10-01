c62c119b33c2288cbebfc21d192560920e0404bd
CarnunMP <carnun@hotmail.co.uk>
2019-10-01 14:04:40 +0100

    App and UserCard fleshed out some

--- project/src/App.js
+++ project/src/App.js
@@ -1,12 +1,64 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import axios from "axios";
+import UserCard from './components/UserCard';
 
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        name: null,
+        username: null,
+        avatarURL: null,
+        location: null,
+        bio: null,
+        followers: null,
+        following: null,
+        profileURL: null,
+      }
+    };
+  }
+
+  componentDidMount() {
+    axios.get("https://api.github.com/users/carnunmp")
+      .then(res => {
+        const user = res.data;
+        this.setState({
+          user: {
+            name: user.name,
+            username: user.login,
+            avatarURL: user.avatar_url,
+            location: user.location,
+            bio: user.bio,
+            followers: user.followers,
+            following: user.following,
+            profileURL: user.url,
+          }
+        })
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <h1>
+            GEThub USER Pages
+          </h1>
+        </div>
+        <UserCard 
+          name={this.state.name}
+          username={this.state.username}
+          avatarURL={this.state.avatarURL}
+          location={this.state.location}
+          bio={this.state.bio}
+          followers={this.state.followers}
+          following={this.state.following}
+          profileURL={this.state.profileURL}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
--- project/src/components/UserCard.js
+++ project/src/components/UserCard.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default class UserCard extends React.Component {
-    
+    render() {
+        const { name, username, avatarURL, location, bio, followers, following, profileURL } = this.props;
+        return (
+            <div className="user-card">
+                <div className="avatar-div">
+                    <img src={avatarURL} />
+                </div>
+            </div>
+        )
+    }
 }
